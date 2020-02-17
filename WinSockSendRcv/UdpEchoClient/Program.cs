68917973873f6f4bc9b1be3aa7d65ba160b22ac4
rezaicando <reza.icando@gmail.com>
2020-02-16 17:38:58 -0800

    First commit for both UdpEchoClient and UdpEchoServer

---
+++ WinSockSendRcv/UdpEchoClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WinSockSendRcv
+{
+    class UdpEchoClient
+    {
+        static void Main(string[] args)
+        {
+            if ((args.Length < 2) || (args.Length > 3))
+            {
+                throw new System.ArgumentException("Parameters: <Server> <Port> <Data>");
+            }
+
+            //Server name or IP Address
+            string server = args[0];
+
+            //Use port argument if entered,otherwise default to 22
+            int servPort = (args.Length == 3) ? Int32.Parse(args[1]) : 22;
+
+            //Convert input string to an array of bytes
+            byte[] sendPacket = Encoding.ASCII.GetBytes(args[2]);
+
+            //Create a udp client instance
+            UdpClient client = new UdpClient();
+
+            try
+            {
+                //Send the Echo String to the specified host and port
+                client.Send(sendPacket, sendPacket.Length, server, servPort);
+
+                Console.WriteLine("sent {0} bytes to the server ...", sendPacket.Length);
+
+                IPEndPoint remoteIPEndpoint = new IPEndPoint(IPAddress.Any, 0);
+
+                //attempt echo reply receive
+                byte[] rcvPacket = client.Receive(ref remoteIPEndpoint);
+
+                Console.WriteLine("Received {0} bytes from {1}: {2}", rcvPacket.Length, remoteIPEndpoint,
+                    Encoding.ASCII.GetString(rcvPacket, 0, rcvPacket.Length));
+
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+            }
+            //close the port
+            client.Close();
+        }
+    }
+}
---
+++ WinSockSendRcv/UdpEchoServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WinSockSendRcv
+{
+    class UdpEchoServer
+    {
+        static void Main(string[] args)
+        {
+            //Test for correct number of args
+            if (args.Length > 1)
+            {
+                throw new ArgumentException("Parameters: <Port>");
+            }
+
+            int servPort = (args.Length == 1) ? Int32.Parse(args[0]) : 22;
+            UdpClient client = null;
+            try
+            {
+                //Create an instance of UdpClient to listen on Port
+                client = new UdpClient(servPort);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                Environment.Exit(se.ErrorCode);
+            }
+
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            //Run receive echo 
+            for (; ; )
+            {
+                try
+                {
+                    //Receive a byte array with echo datagram packet content
+                    byte[] byteBuffer = client.Receive(ref remoteIPEndPoint);
+                    Console.Write("Client at " + remoteIPEndPoint + " - ");
+
+                    //Send echo packet back to client
+                    client.Send(byteBuffer, byteBuffer.Length, remoteIPEndPoint);
+                    Console.WriteLine("echoed {0} bytes.", byteBuffer.Length);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                }
+            }
+        }
+    }
+}
